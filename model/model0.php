0ba386bb096cbb197698c3d88d46f57ad975ea06
lordders <lordders93@hotmail.com>
2015-10-06 11:13:18 +0000

    0

--- model/model0.php
+++ model/model0.php
@@ -3,7 +3,7 @@ class Model {
     public $string;
     
     public function __construct() {
-        $this->string = "MVC + PHP = Awesome! Ruben";
+        $this->string = "MVC + PHP = Awesome!";
     }
 }
 ?>
