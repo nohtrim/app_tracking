501c680bc88695979f4ac018eb59f4024865eb3d
lordders <lordders93@hotmail.com>
2015-10-06 11:27:21 +0000

    aa

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    include_once("model/model.php");
+    include_once("controller/controller.php");
+    include_once("view/view.php");
+    
+    $model = new Model();
+    $controller = new Controller($model);
+    $view = new View($controller, $model);
+    
+    if (isset($_GET['action']) && !empty($_GET['action'])) {
+        $controller->{$_GET['action']}();
+    }
+    echo $view->output();
+?>
