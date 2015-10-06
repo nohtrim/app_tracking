d783e708f35d367f843472333d284f40872a7bc9
IosuR <irecaldema14dw@ikzubirimanteo.com>
2015-10-06 10:48:51 +0000

    0

--- ejemplo.php
+++ ejemplo.php
@@ -1,7 +1,7 @@
 <?php
-include_once(".model/model0.php");
-include_once(".controller/controller0.php");
-include_once(".view/view0.php");
+include_once("model/model0.php");
+include_once("controller/controller0.php");
+include_once("view/view0.php");
 $model = new Model();
 $controller = new Controller($model);
 $view = new View($controller, $model);
@@ -9,3 +9,4 @@ if (isset($_GET['action']) && !empty($_GET['action'])) {
     $controller->{$_GET['action']}();
 }
 echo $view->output();
+?>
